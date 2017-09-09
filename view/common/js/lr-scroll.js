e00a4c8e8215783cfc8a0521f9d409ec82f20b19
fengyong <373209197@qq.com>
2017-09-09 23:15:09 +0800

    cao

--- view/common/js/lr-scroll.js
+++ view/common/js/lr-scroll.js
@@ -107,7 +107,7 @@
             downList = downList || $(".navList li:eq(0) .downList");
             if(downList){
                 $(downList).css({
-                    'height':downList.children().length * 23 + 'px',
+                    'height':downList.children().length * 30 + 'px',
                     'opacity':1,
                     'margin-top':'5px'
                 })
